4cde14f0fc56126f49d5d5abbc8b9ec2f0804cd4
KrystynaFedorova <104518844+KrystynaFedorova@users.noreply.github.com>
2022-07-23 18:03:01 +0200

    redo 2task

--- Lesson8(hw5)/2task.js
+++ Lesson8(hw5)/2task.js
@@ -4,33 +4,53 @@
 // Функція має повертати результат розрахунку
 
 
-function power(N, S) {
-    let pow = 1
-    if (S >= 0) {
-        for (let i = 0; i < S; i++) {
-            pow = N * pow
-        }
-        console.log(pow)
-    } else {
-        for (let j = 0; j > S; j--) {
-            pow = N * pow
-        }
-        console.log(1 / pow)
-    }
-}
-power(2, -3)
+// function power(N, S) {
+//     let pow = 1
+//     if (S >= 0) {
+//         for (let i = 0; i < S; i++) {
+//             pow = N * pow
+//         }
+//         console.log(pow)
+//     } else {
+//         for (let j = 0; j > S; j--) {
+//             pow = N * pow
+//         }
+//         console.log(1 / pow)
+//     }
+// }
+// power(2, -3)
+//
+//
+// function pow(n, s) {
+//     if (s === 0) {
+//         return 1
+//     }
+//     if (s > 0) {
+//         return (n * pow(n, s - 1))
+//     } else {
+//         // return (1 / n * pow(n, s + 1))
+//         return (1 / pow(n, -s))
+//     }
+// }
+// console.log(pow(6, -3))
+//
 
 
-function pow(n, s) {
-    if (s === 0) {
-        return 1
-    }
-    if (s > 0) {
-        return (n * pow(n, s - 1))
-    } else {
-        // return (1 / n * pow(n, s + 1))
-        return (1 / pow(n, -s))
+function power2(N) {
+    return(S)=> {
+        let pow = 1
+        if (S >= 0) {
+            for (let i = 0; i < S; i++) {
+                pow = N * pow
+            }
+            return(pow)
+        } else {
+            for (let j = 0; j > S; j--) {
+                pow = N * pow
+            }
+            return(1 / pow)
+        }
     }
 }
-console.log(pow(6, -3))
+console.log(power2(2)(5))
 
